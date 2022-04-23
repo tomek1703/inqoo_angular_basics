65f0066f9ad88531e12eaa58e72e9f90676ec31f
Michał Witek <michal.witek.katowice@gmail.com>
2022-04-23 11:00:59 +0200

    [ngClass] example

--- inqoo_angular/src/app/shared/components/inqoo-button/inqoo-button.component.ts
+++ inqoo_angular/src/app/shared/components/inqoo-button/inqoo-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-inqoo-button',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InqooButtonComponent implements OnInit {
 
-  constructor() { }
+  @Input() size: 'small' | 'large' | 'default' = "default";
 
-  ngOnInit(): void {
+  ngOnInit() {
+      console.log(this.size)
   }
 
+  isSmall = () => this.size === 'small';
+  isLarge = () => this.size === 'large';
 }
