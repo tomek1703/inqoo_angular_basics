0a0fd1d989752769ec74b57852f80e6b4427c528
Michał Witek <michal.witek.katowice@gmail.com>
2022-04-24 11:19:53 +0200

    init routing configuration

--- inqoo_angular/src/app/app-routing.module.ts
+++ inqoo_angular/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SwStarshipsPageComponent} from "./sw-starships/pages/sw-starships-page/sw-starships-page.component";
+import {SwPeoplePageComponent} from "./sw-people/pages/sw-people-page/sw-people-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'sw-starships', component: SwStarshipsPageComponent
+}, {
+  path: 'sw-people', component: SwPeoplePageComponent
+},
+  // {path: '', redirectTo: '/sw-starships', pathMatch: 'full'},
+  { path: '**', component: SwPeoplePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
